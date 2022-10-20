5a587bbf81381b3320f4abf218fdf69dac6084cf
AdmiralXy <gameproblem80@gmail.com>
2022-10-20 04:19:15 +0300

    add third-party api for country service

--- countries-service/src/main/java/com/admiralxy/countries/CountriesApplication.java
+++ countries-service/src/main/java/com/admiralxy/countries/CountriesApplication.java
@@ -1,17 +1,24 @@
 package com.admiralxy.countries;
 
 import com.admiralxy.countries.clients.CountriesClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @EnableFeignClients
 @SpringBootApplication
 public class CountriesApplication implements CountriesClient {
+
+    @Value("${spring.application.third-party-api}")
+    private String thirdPartyApi;
 
     public static void main(String[] args) {
         SpringApplication.run(CountriesApplication.class, args);
@@ -19,7 +26,12 @@ public class CountriesApplication implements CountriesClient {
 
     @GetMapping("/countries-management/countries/{country-name}")
     @Override
-    public String getCountry(@PathVariable("country-name") String countryName) {
-        return "Country: " + countryName;
+    public ResponseEntity<?> getByCountry(@PathVariable("country-name") String countryName) {
+        RestTemplate restTemplate = new RestTemplate();
+        try {
+            return restTemplate.getForEntity(thirdPartyApi + countryName, String.class);
+        } catch (RestClientException exception) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
--- countries-service/src/main/java/com/admiralxy/countries/clients/CountriesClient.java
+++ countries-service/src/main/java/com/admiralxy/countries/clients/CountriesClient.java
@@ -1,8 +1,9 @@
 package com.admiralxy.countries.clients;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
 
 @FeignClient("countries-eureka-client")
 public interface CountriesClient {
-    String getCountry(String countryName);
+    ResponseEntity<?> getByCountry(String countryName);
 }
