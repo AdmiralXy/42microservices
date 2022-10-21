7b90e867d73ca5848db7dab5f3e731211253f1d2
AdmiralXy <gameproblem80@gmail.com>
2022-10-21 03:39:18 +0300

    add empty result handle

--- covid-service/src/main/java/com/admiralxy/covid/CovidApplication.java
+++ covid-service/src/main/java/com/admiralxy/covid/CovidApplication.java
@@ -1,5 +1,6 @@
 package com.admiralxy.covid;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @RestController
 @SpringBootApplication
@@ -25,7 +28,11 @@ public class CovidApplication {
     public ResponseEntity<String> getByCountry(@PathVariable("country-name") String countryName) {
         RestTemplate restTemplate = new RestTemplate();
         try {
-            return restTemplate.getForEntity(thirdPartyApi + countryName, String.class);
+            ObjectNode response = restTemplate.getForObject(thirdPartyApi + countryName, ObjectNode.class);
+            if (Objects.requireNonNull(response).get("data").isEmpty()) {
+                return ResponseEntity.badRequest().build();
+            }
+            return ResponseEntity.ok(response.get("data").toString());
         } catch (RestClientException exception) {
             return ResponseEntity.badRequest().build();
         }
