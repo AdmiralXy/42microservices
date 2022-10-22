b9bc68d3ad0f93c95a0103647d6b6af9b3df0b1c
AdmiralXy <gameproblem80@gmail.com>
2022-10-22 17:45:50 +0300

    remove empty result protection

--- covid-service/src/main/java/com/admiralxy/covid/CovidApplication.java
+++ covid-service/src/main/java/com/admiralxy/covid/CovidApplication.java
@@ -1,6 +1,5 @@
 package com.admiralxy.covid;
 
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.Objects;
 
 @RestController
 @SpringBootApplication
@@ -28,11 +25,7 @@ public class CovidApplication {
     public ResponseEntity<String> getByCountry(@PathVariable("country-name") String countryName) {
         RestTemplate restTemplate = new RestTemplate();
         try {
-            ObjectNode response = restTemplate.getForObject(thirdPartyApi + countryName, ObjectNode.class);
-            if (Objects.requireNonNull(response).get("data").isEmpty()) {
-                return ResponseEntity.badRequest().build();
-            }
-            return ResponseEntity.ok(response.get("data").toString());
+            return restTemplate.getForEntity(thirdPartyApi + countryName, String.class);
         } catch (RestClientException exception) {
             return ResponseEntity.badRequest().build();
         }
